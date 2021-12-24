b586ad592bff6b5cbe4cbe6a9ab576038529f7d8
David <davcacrom@alum.us.es>
2021-12-24 16:03:31 +0100

    Changed db url

--- db.js
+++ db.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
-
-var DB_URL = ('mongodb://localhost:27017/test');
-
-if(process.env.MONGO_HOSTNAME!=undefined){
-    DB_URL = ('mongodb://'+process.env.MONGO_HOSTNAME+':27017/test');
-}else{
-    DB_URL = (process.env.MONGO_URL|| 'mongodb://localhost:27017/test');
-}
+const DB_URL = (process.env.MONGO_URL || 'mongodb://mongo/test');
 
 const dbConnect = function() {
+    console.log("Trying to connect to: " + DB_URL);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
     return mongoose.connect(DB_URL, { useNewUrlParser: true });
