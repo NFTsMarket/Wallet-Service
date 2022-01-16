f65a59df6d1ccdeaaa8c632d89933e510efb39ec
David <davcacrom@alum.us.es>
2022-01-16 11:24:04 +0100

    Fixed port to deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server.js');
 const dbConnect = require('./db');
 
-var port = (process.env.PORT || 3001);
+var port = (process.env.PORT || 3000);
 
 console.log("Starting API server at "+port);
 
